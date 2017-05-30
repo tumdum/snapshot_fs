925c519c088d646814f0c07205557fedd1a6725f
Tomasz Kłak <tomasz@tomaszklak.pl>
2017-05-30 21:52:14 +0200

    Added tarfs as supported archive in main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"github.com/hanwen/go-fuse/fuse/pathfs"
@@ -31,17 +32,25 @@ func failOnErr(format string, err error) {
 func main() {
 	flag.Parse()
 	if len(flag.Args()) < 2 {
-		log.Fatal("Usage:\n  snapshot_fs MOUNTPOINT PATH_TO_ZIP")
+		log.Fatal("Usage:\n  snapshot_fs MOUNTPOINT ARCHIVE")
 	}
 
 	f, err := os.Open(flag.Arg(1))
-	failOnErr("Could not opne zip file: %v", err)
+	failOnErr("Could not opne file: %v", err)
 
 	stat, err := f.Stat()
-	failOnErr("Could not stat zip file: %v", err)
+	failOnErr("Could not stat file: %v", err)
 
-	fs, err := NewZipFs(f, stat.Size())
-	failOnErr("Could not read zip file: %v", err)
+	var fs pathfs.FileSystem
+	if strings.HasSuffix(flag.Arg(1), ".zip") {
+		tmp, err := NewZipFs(f, stat.Size())
+		failOnErr("Could not read zip file: %v", err)
+		fs = tmp
+	} else if strings.HasSuffix(flag.Arg(1), ".tar") {
+		tmp, err := NewTarFs(f)
+		failOnErr("Could not read tar file: %v", err)
+		fs = tmp
+	}
 
 	nfs := pathfs.NewPathNodeFs(fs, nil)
 	server, _, err := nodefs.MountRoot(flag.Arg(0), nfs.Root(), nil)
