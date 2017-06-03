be48e6645876640e21b661511a31dc76989bdf97
Tomasz Kłak <tomasz@tomaszklak.pl>
2017-06-03 13:54:12 +0200

    tarfs is a dir

--- tarfs.go
+++ tarfs.go
@@ -11,47 +11,7 @@ import (
 )
 
 type tarfs struct {
-	root dir
-}
-
-func (t *tarfs) setName(name string) {
-	t.root.setName(name)
-}
-
-func (t *tarfs) name() string {
-	return t.root.name()
-}
-
-func (t *tarfs) files() []file {
-	return t.root.files()
-}
-
-func (t *tarfs) dirs() []dir {
-	return t.root.dirs()
-}
-
-func (t *tarfs) addEmptyDir(name string) dir {
-	return t.root.addEmptyDir(name)
-}
-
-func (t *tarfs) addDir(d dir) dir {
-	return t.root.addDir(d)
-}
-
-func (t *tarfs) setRecursiveDir(name string, d dir) bool {
-	return t.root.setRecursiveDir(name, d)
-}
-
-func (t *tarfs) addFile(f file) file {
-	return t.root.addFile(f)
-}
-
-func (t *tarfs) findDir(name string) dir {
-	return t.root.findDir(name)
-}
-
-func (t *tarfs) findFile(name string) file {
-	return t.root.findFile(name)
+	dir
 }
 
 func newDirFromTar(r io.ReadSeeker) (dir, error) {
